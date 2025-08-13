6ed78b371e7423ad992bbb0b01d2e2f63a5af9b4
narodeSiddhant <siddhantnarode@gmail.com>
2025-08-12 23:19:52 -0400

    updated password

--- index.js
+++ index.js
@@ -13,7 +13,7 @@ import mongoose from "mongoose";
 import "dotenv/config";
 
 const CONNECTION_STRING =
-  process.env.MONGO_CONNECTION_STRING || "mongodb+srv://giuseppi:<db_password>@kambaz.bjusetp.mongodb.net/?retryWrites=true&w=majority&appName=Kambaz";
+  process.env.MONGO_CONNECTION_STRING || "mongodb+srv://giuseppi:<supersecretpassword>@kambaz.bjusetp.mongodb.net/?retryWrites=true&w=majority&appName=Kambaz";
 mongoose.connect(CONNECTION_STRING);
 
 const app = express();

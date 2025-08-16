a68abc670ec87ff088f321118db7cf88353d5e74
narodeSiddhant <siddhantnarode@gmail.com>
2025-08-16 08:37:48 -0400

    schema updates

--- Kanbas/Users/schema.js
+++ Kanbas/Users/schema.js
@@ -1,6 +1,7 @@
 import mongoose from "mongoose";
 const userSchema = new mongoose.Schema(
   {
+    _id: { type: String }, // Explicitly define _id as String type
     username: { type: String, required: true, unique: true },
     password: { type: String, required: true },
     firstName: String,
